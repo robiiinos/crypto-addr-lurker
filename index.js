ee2f09320f3c62bd53e11b7af7578a9af27389fe
D. Robin <dyrda.robin@outlook.fr>
2018-09-17 21:24:50 +0200

    Base app for ZMQ communication

---
+++ index.js
@@ -0,0 +1,9 @@
+import zeromq from './src/zeromq';
+import config from './config';
+
+zeromq.connect();
+zeromq.suscribe();
+
+zeromq.onMessage((topic, message) => {
+    console.log(`${topic.toString()} : ${message.toString('hex')}`);
+});
---
+++ src/zeromq.js
@@ -0,0 +1,25 @@
+import zeromq from 'zeromq';
+import config from '../config';
+
+const sock = zeromq.socket('sub');
+
+function connect() {
+    sock.connect(`${config.node.protocol}://${config.node.host}:${config.node.port}`);
+}
+
+function suscribe() {
+    sock.subscribe('hashtx');
+}
+
+function onMessage(callback) {
+    sock.on('message', (topic, message) => {
+        callback(topic, message);
+    });
+}
+
+
+export default {
+    connect,
+    suscribe,
+    onMessage
+};
